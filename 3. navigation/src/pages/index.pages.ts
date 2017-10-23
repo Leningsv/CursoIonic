78a5576c7eb957b73ca819ae90f53267a53a225b
lenin <leningsv@hotmail.com>
2017-10-23 11:38:27 -0500

    ultimos cambios

---
+++ 3. navigation/src/pages/index.pages.ts
@@ -0,0 +1,8 @@
+export { MainPage } from "./main/main";
+export { SettingsPage } from "./settings/settings";
+export { Settings2Page } from "./settings2/settings2";
+export { ModalPage } from "./modal/modal";
+export { Page1Page } from "./page1/page1";
+export { Page2Page } from "./page2/page2";
+export { Page3Page } from "./page3/page3";
+export { TabsPage } from "./tabs/tabs";
