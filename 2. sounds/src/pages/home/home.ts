37894b6b97575b31d6f56df1c4eb684dc90354a9
Lenin Guillero Samaniego Vizcaino <leningsv@hotmail.com>
2018-04-11 10:12:51 -0500

    Ultimos cambios

--- 2. sounds/src/pages/home/home.ts
+++ 2. sounds/src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  animals = ANIMALES.slice(0);
   constructor(public navCtrl: NavController) {
 
   }
-
+  play(item: any) {
+    console.log(item);
+  }
 }
